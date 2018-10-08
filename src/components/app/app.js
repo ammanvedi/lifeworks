95a90a2fcdd3c3c602ec99568d587d0629439017
Amman Vedi <ammanvedi@Ammans-MacBook-Pro.local>
2018-10-08 09:11:39 +0100

    add responsive styles to app

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './app.scss';
 
-import BalanceContainer from './container/BalanceContainer';
+import BalanceContainer from './container/BalanceContainer/BalanceContainer';
 
 class App extends React.Component {
   render() {
