66777136afde212039d86504634a15f0e9f322cd
Amman Vedi <ammanvedi@Ammans-MacBook-Pro.local>
2018-10-08 09:05:00 +0100

    create constants file

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,1 +1,18 @@
 export const BALANCE_URL = 'https://codepen.io/andismith/pen/oqzxyP.js';
+
+export const LOWEST_DENOMINATION_MULTIPLIER = 100;
+
+export const UI_STRINGS = {
+    BALANCE_NEGATIVE: 'Your balance is negative!',
+    UPDATE_PLACEHOLDER: 'Add a value',
+    UPDATE_ERROR: 'Value should be non zero and entered in a valid currency format e.g. 15, 0.5, 4.99, 0.01',
+    TRANSACTIONS_NONE: 'You have no previous balance history',
+    WALLET: 'Wallet',
+    BALANCE_HISTORY: 'Balance History'
+}
+
+export const VALID_INPUT_REGEX = /^[0-9]+(\.[0-9]{1,2})?$/;
+
+export const CURRENCY_MAP = {
+    '£': 'GBP'
+};
